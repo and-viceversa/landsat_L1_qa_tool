0eebe217dd88b085268885ffc12fb7eaa640e0d1
Adam <25871157+and-viceversa@users.noreply.github.com>
2019-04-27 22:41:52 -0700

    Added methods for code reusability. Fixed _bqa.tiff extension for interoperability with USGS EROS output..

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="landsat_L1_qa_tool",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     scripts=['landsat_L1_qa_tool.py'],
     author="AdamR",
